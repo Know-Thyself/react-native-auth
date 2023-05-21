20397fc2409f54d45d1fee3344bf61db3a522ccc
Biruk Kebede <biruk.key@gmail.com>
2023-05-21 17:47:17 +0100

    async storage implementation to auto sign in

--- context/authContext.js
+++ context/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const AuthContext = createContext({
   token: '',
@@ -9,19 +10,34 @@ export const AuthContext = createContext({
 
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState()
-  const [isAuthorized, setIsAuthorized] = useState(false)
+
+  useEffect(() => {
+    const retrieveToken = async () => {
+      const storedToken = await AsyncStorage.getItem('token')
+      if (storedToken) {
+        setAuthToken(storedToken)
+      }
+    }
+    retrieveToken()
+  }, [])
+
   const authenticate = token => {
     setAuthToken(token)
+    AsyncStorage.setItem('token', token)
   }
+
   const signOut = () => {
     setAuthToken(null)
+    AsyncStorage.removeItem('token')
   }
+
   const state = {
     token: authToken,
     isAuthenticated: !!authToken,
     authenticate,
     signOut,
   }
+
   return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>
 }
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,17 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const WelcomeScreen = () => {
   const [message, setMessage] = useState('')
+  const { token } = useContext(AuthContext)
 
   useEffect(() => {
     fetch(
-      'https://auth-practice-51a0b-default-rtdb.europe-west1.firebasedatabase.app/message.json'
+      `https://auth-practice-51a0b-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`
     )
       .then(response => response.json())
       .then(data => setMessage(data))
       .catch(err => console.error(err))
-  }, [])
+  }, [token])
 
   return (
     <View style={styles.rootContainer}>
